77c0a3176abcb025e6111f4b3ca122bdb7051f9a
Nekoniyah <beastish.maki@gmail.com>
2026-05-11 18:16:21 +0200

    removed events loading in index in favor of modules

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import "dotenv/config";
-import { Client, Partials, type ClientEvents } from "discord.js";
+import { Client, Partials } from "discord.js";
 import loadDirectoryList from "./utils/loadDirectoryList";
 import path from "node:path";
-import type { TemplateEvent } from "./utils/typers";
 import type ModuleBuilder from "./utils/module/ModuleBuilder";
 
 const client = new Client({
@@ -11,28 +10,12 @@ const client = new Client({
 });
 
 (async () => {
-  const eventsPath = path.join(process.cwd(), "events");
-  const events = await loadDirectoryList(eventsPath, eventsPath);
-
   const modulesPath = path.join(process.cwd(), "modules");
   const modules = await loadDirectoryList(modulesPath, modulesPath);
 
   for (let key in modules) {
     for (let path of modules[key]!) {
-      const { default: module } = (await import(path)) as {
-        default: ModuleBuilder;
-      };
-    }
-  }
-
-  for (let key in events) {
-    for (let path of events[key]!) {
-      try {
-        const { default: event } = await import(path);
-        client.on(key, event as typeof TemplateEvent<keyof ClientEvents>);
-      } catch (e) {
-        console.error(e);
-      }
+      await import(path);
     }
   }
 
